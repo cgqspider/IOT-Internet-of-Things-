f1b4e6fb5957287f51875b1c494d2c46f5d769c2
Narayan Jha <cgqspider@gmail.com>
2019-10-08 15:42:45 +0530

    done

---
+++ IBM/Nodemcu/esp8266code.ino
@@ -0,0 +1,117 @@
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h> // https://github.com/knolleary/pubsubclient/releases/tag/v2.3
+#include <ArduinoJson.h> // https://github.com/bblanchon/ArduinoJson/releases/tag/v5.0.7
+
+//-------- Customise these values -----------
+const char* ssid = "T2TECHHUB";
+const char* password = "T2T@12345";
+
+#define ORG "ic01na"
+#define DEVICE_TYPE "NodeMcu"
+#define DEVICE_ID "demoDevice"
+#define TOKEN "JZp5ytS*_)H!tZf*EM"
+//-------- Customise the above values --------
+
+char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
+char authMethod[] = "use-token-auth";
+char token[] = TOKEN;
+char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
+
+const char eventTopic[] = "iot-2/evt/status/fmt/json";
+const char cmdTopic[] = "iot-2/cmd/led/fmt/json";
+
+
+
+WiFiClient wifiClient;
+void callback(char* topic, byte* payload, unsigned int payloadLength) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < payloadLength; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if (payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+PubSubClient client(server, 1883, callback, wifiClient);
+
+int publishInterval = 5000; // 5 seconds//Send adc every 5sc
+long lastPublishMillis;
+
+void setup() {
+  Serial.begin(9600); Serial.println();
+  pinMode(LED_BUILTIN, OUTPUT);
+  wifiConnect();
+  mqttConnect();
+}
+
+void loop() {
+  if (millis() - lastPublishMillis > publishInterval) {
+    publishData();
+    lastPublishMillis = millis();
+  }
+
+  if (!client.loop()) {
+    mqttConnect();
+  }
+}
+
+void wifiConnect() {
+  Serial.print("Connecting to "); Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("nWiFi connected, IP address: "); Serial.println(WiFi.localIP());
+
+}
+
+void mqttConnect() {
+  if (!!!client.connected()) {
+    Serial.print("Reconnecting MQTT client to "); Serial.println(server);
+    while (!!!client.connect(clientId, authMethod, token)) {
+      Serial.print(".");
+      delay(500);
+    }
+    if (client.subscribe(cmdTopic)) {
+      Serial.println("subscribe to responses OK");
+    } else {
+      Serial.println("subscribe to responses FAILED");
+    }
+    Serial.println();
+  }
+}
+
+
+void publishData() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+
+  String payload = "{\"d\":{\"adc\":";
+  payload += String(sensorValue, DEC);
+  payload += "}}";
+
+  Serial.print("Sending payload: "); Serial.println(payload);
+
+  if (client.publish(eventTopic, (char*) payload.c_str())) {
+    Serial.println("Publish OK");
+  } else {
+    Serial.println("Publish FAILED");
+  }
+}
+
+
+
+
+
