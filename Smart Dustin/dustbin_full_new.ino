f6fba16565bc1c7474464fa70d69a182230ecbc1
Narayan Jha <52191223+cgqspider@users.noreply.github.com>
2019-10-20 12:10:16 +0530

    Project Smart dustbin added

---
+++ Smart Dustin/dustbin_full_new.ino
@@ -0,0 +1,111 @@
+#include "TinyGPS++.h"
+#include "SoftwareSerial.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+// defines pins numbers
+const int trigPin = 4;
+const int echoPin = 5;
+// defines variables
+long duration;
+int distance;
+
+const char* ssid = "monu";
+const char* password = "";
+
+
+SoftwareSerial serial_connection(12, 13); //RX=pin 12, TX=pin 13
+TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
+void setup()
+{
+
+  Serial.begin(9600);//This opens up communications to the Serial monitor in the Arduino IDE
+  serial_connection.begin(9600);//This opens up communications to the GPS
+  Serial.println("GPS Start");//Just show to the monitor that the sketch has started
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+
+WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+ 
+    delay(1000);
+    Serial.print("Connecting..");
+ 
+  }
+}
+
+void loop()
+{
+  while(serial_connection.available())//While there are characters to come from the GPS
+  {
+    gps.encode(serial_connection.read());//This feeds the serial NMEA data into the library one char at a time
+  }
+
+ 
+
+  if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
+  {
+ 
+    
+  // Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+
+
+
+    
+    //Get the latest info from the gps object which it derived from the data sent by the GPS unit
+    Serial.println("Satellite Count:");
+    Serial.println(gps.satellites.value());
+    Serial.println("Latitude:");
+    Serial.println(gps.location.lat(), 6);
+    Serial.println("Longitude:");
+    Serial.println(gps.location.lng(), 6);
+    Serial.println("Speed MPH:");
+    Serial.println(gps.speed.mph());
+    Serial.println("Altitude Feet:");
+    Serial.println(gps.altitude.feet());
+    Serial.println("");
+
+Serial.print("------------------------------------------");
+// Prints the distance on the Serial Monitor
+Serial.print("\nDistance: ");
+Serial.println(distance);
+
+
+
+
+ if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+ 
+    HTTPClient http;  //Declare an object of class HTTPClient 
+    http.begin("http://reverenced-depositi.000webhostapp.com/insert.php?lat="+String(gps.location.lat())+"&lon="+String(gps.location.lng())+"&dustbin_sts="+String(distance));  //Specify request destination
+    int httpCode = http.GET();                                                                  //Send the request
+ 
+    if (httpCode > 0) { //Check the returning code
+ 
+      String payload = http.getString();   //Get the request response payload
+      Serial.println(payload);                     //Print the response payload
+ 
+    }
+ 
+    http.end();   //Close connection
+ 
+  }
+
+   delay(10000);
+
+  }
+
+
+}
+
+
---
+++ Smart Dustin/smart_dustbin_own_server/smart_dustbin_own_server.ino
@@ -0,0 +1,72 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+const int trigPin = 4;  //D4
+const int echoPin = 5;  //D3
+
+long duration;
+int distance,percentage;
+
+ int  total_len;
+const char* ssid = "monu";
+const char* password = "";
+ 
+void setup () {
+ pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+total_len=19;
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+ 
+    delay(1000);
+    Serial.print("Connecting..");
+ 
+  }
+ 
+}
+ 
+void loop() {
+
+
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance= duration*0.034/2;
+Serial.print("Distance: ");
+Serial.println(distance);
+ 
+  percentage=(distance*100)/total_len; 
+
+
+ 
+  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+ 
+    HTTPClient http;  //Declare an object of class HTTPClient
+
+   
+
+
+ 
+    http.begin("http://reverenced-depositi.000webhostapp.com/insert.php?temp=3&hum="+String(percentage));  //Specify request destination
+    int httpCode = http.GET();                                                                  //Send the request
+ 
+    if (httpCode > 0) { //Check the returning code
+ 
+      String payload = http.getString();   //Get the request response payload
+      Serial.println(payload);                     //Print the response payload
+ 
+    }
+ 
+    http.end();   //Close connection
+ 
+  }
+
+
+  delay(10000);    //Send a request every 30 seconds
+ 
+}
